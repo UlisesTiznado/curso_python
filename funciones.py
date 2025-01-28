f1d527b2663b4975b0c79dba19fdd686852ba671
UlisesTiznado <molinojutr02@hotmail.com>
2025-01-28 09:46:51 -0700

    Agregue nuevas funciones para ir al corriente

--- funciones.py
+++ funciones.py
@@ -1,12 +1,32 @@
-def calcular_y(x:float,m:float,b:float)->float:
+def Calcular_Y(x: float,m: float ,b: float) -> float:
+    '''
+    Calcula el valor de y en una linea recta
+    x: valor de x
+    m: pendiente
+    b: interseccion de y
+    regresa el valor de y
+    '''
     return m*x+b
-
-def test_linea():
-    y = calcular_y(0,2.0,3.0)
-    return y
-
+ 
+def main():
+    m=2
+    b=3
+    x=5
+    y= Calcular_Y(x,m,b)
+    print(f'Para x={x}, y={y}')
+ 
 if __name__ == '__main__':
-    if test_linea()==3.0:
-        print('Test Exitoso')
+    main()
+ 
+def test_linea():
+    '''
+    Prueba de funcionamiento de calcular_Y
+    '''
+    y = Calcular_Y(0.0,2.0,3.0)
+    return y
+ 
+if __name__ == '__main__':
+    if test_linea() == 3.0:
+        print('everything aight')
     else:
-        print('Test Fallido')
+        print('sum went wrong')
